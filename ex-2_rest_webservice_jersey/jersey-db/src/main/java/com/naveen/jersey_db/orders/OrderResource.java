c5ae82d0f2b2a52c2933d1ff0a78567493829f70
naveenkumar <www.naveenkumar24637@gmail.com>
2022-05-31 20:43:53 +0530

    orders module BOLA cleared

--- ex-2_rest_webservice_jersey/jersey-db/src/main/java/com/naveen/jersey_db/orders/OrderResource.java
+++ ex-2_rest_webservice_jersey/jersey-db/src/main/java/com/naveen/jersey_db/orders/OrderResource.java
@@ -1,7 +1,10 @@
 package com.naveen.jersey_db.orders;
 
+import com.naveen.jersey_db.exception.CustomException;
+import com.naveen.jersey_db.util.UserUtils;
 import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -20,13 +23,17 @@ public class OrderResource {
     @Path("/{id}")
     @GET
     @PermitAll
-    public List<Orders> getOrders(@PathParam("id") int id) {
+    public List<Orders> getOrders(@PathParam("id") int id,
+                                  @HeaderParam(HttpHeaders.AUTHORIZATION) String authorization) {
+        if (!UserUtils.compareUserIdAuthId(authorization, String.valueOf(id))) throw new CustomException("Id didn't match");
         return service.getOrders(id);
     }
 
     @Path("/{id}")
     @POST
-    public Response orderNow(@PathParam("id") int id) {
+    public Response orderNow(@PathParam("id") int id,
+                             @HeaderParam(HttpHeaders.AUTHORIZATION) String authorization) {
+        if (!UserUtils.compareUserIdAuthId(authorization, String.valueOf(id))) throw new CustomException("Id didn't match");
         service.orderNow(id);
         return Response.ok().build();
     }
